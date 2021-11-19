85773e580305e6a74135dce521579f7151d6a121
Karan <makeshift007@gmail.com>
2021-11-19 23:23:45 +0530

    Added Resources

--- PizzaSystem/PizzaSystem.API/Startup.cs
+++ PizzaSystem/PizzaSystem.API/Startup.cs
@@ -31,6 +31,7 @@ namespace PizzaSystem.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddSwaggerGen();
 
@@ -61,8 +62,12 @@ namespace PizzaSystem.API
             {
                 app.UseHsts();
             }
-
+            app.UseStaticFiles();
             app.UseHttpsRedirection();
+            app.UseCors(builder => builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
             app.UseMvc();
             app.UseSwagger();
             app.UseSwaggerUI(c =>
